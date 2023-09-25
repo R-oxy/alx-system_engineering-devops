4aec097ee2f1bc1f7b0b485896f7ddeee3d275b8
R-oxy <aronmangati@gmail.com>
2023-09-24 23:06:37 -0700

    new updates

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Client configuration only through SSH keys
+include stdlib
+
+file_line { 'Disable passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication'
+}
+
+file_line { 'Identity file declaration':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^IdentityFile'
+}
